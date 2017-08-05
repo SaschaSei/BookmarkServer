b8c3ad4e8c9bdf9d4cb99e26ccbd7d051384367d
SaschaSei <saschasei@googlemail.com>
2017-08-05 19:10:54 +0200

    Change to ThreadedServer

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -45,6 +45,8 @@ import http.server
 import requests
 import os
 from urllib.parse import unquote, parse_qs
+import threading
+from socketserver import ThreadingMixIn
 
 memory = {}
 
@@ -82,6 +84,8 @@ def CheckURI(uri, timeout=30):
     except requests.RequestException:
         return False
 
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurcency."
 
 class Shortener(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -151,5 +155,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000)) #Use PORT if it is there
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
